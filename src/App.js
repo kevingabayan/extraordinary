1e17e279872fea07bd404bfc7d06d22f5cbed18e
Kevin Gabayan <kevin.gabayan@stonybrook.edu>
2021-01-05 21:33:54 -0500

    Added temp navbar and simple layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
+import { Navbar, Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Extraordinary
-        </p>
-      </header>
+      <Navbar className="App-nav" fixed="top">
+        <Navbar.Brand className="App-nav-left">Navbar</Navbar.Brand> 
+      </Navbar>
+      <Container>
+        <header className="App-header">
+          <p>
+            Extraordinary
+          </p>
+        </header>
+        <header className="App-header">
+          <p>
+            The Only Thing
+          </p>
+        </header>
+        <header className="App-header">
+          <p>
+            Road Trip
+          </p>
+        </header>
+      </Container>
+
     </div>
   );
 }
